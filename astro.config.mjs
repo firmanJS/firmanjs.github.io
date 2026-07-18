16561fe31a61151d13d70090cd3af1881ca77a58
firmanJS <fimzhakim@gmail.com>
2026-07-18 15:58:24 +0700

    fix: clean up sitemap XML formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -57,7 +57,7 @@ export default defineConfig({
             });
 
           const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" >
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls.map(u => `  <url>
     <loc>${u.loc}</loc>
     <lastmod>${u.lastmod}</lastmod>
